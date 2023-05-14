8f4d29400a18cb48fbd3530238928fb868bd487f
sunrabbit123 <qudwls185@naver.com>
2023-05-14 16:47:52 +0900

    Initial commit

---
+++ npm/darwin-x64/README.md
@@ -0,0 +1,3 @@
+# `espresso-darwin-x64`
+
+This is the **x86_64-apple-darwin** binary for `espresso`
---
+++ sample/1.hello-world/index.js
@@ -0,0 +1,13 @@
+var espresso = require("../../");
+
+var app = (module.exports = espresso());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express started on port 3000");
+}
